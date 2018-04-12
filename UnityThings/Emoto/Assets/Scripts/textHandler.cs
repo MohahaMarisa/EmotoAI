048157d8d3a44b3454b109418a80b055ca22e6e7
marisa <contactmarisalu@gmail.com>
2018-04-12 15:11:29 -0400

    just changed emoto unity foile to unity things foler

---
+++ UnityThings/Emoto/Assets/Scripts/textHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class textHandler : MonoBehaviour {
+	const string fileName = "movements.txt";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OutputMovement () {
+		using (BinaryWriter writer = new BinaryWriter (File.Open (fileName, FileMode.Create))) {
+			writer.Write (123);
+			writer.Write (123.456f);
+			writer.Write ("Hello, World!");
+			writer.Write(true);
+		}
+	}
+}
